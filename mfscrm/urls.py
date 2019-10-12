6c75d0d4d35e30d803fbfb177385e09f875d1bdd
kjanderson-uno <kjanderson@unomaha.edu>
2019-10-11 22:14:11 -0500

    First commit

---
+++ mfscrm/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('crm.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
